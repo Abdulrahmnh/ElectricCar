70e72aa09a0c397ffe04885c43e286d5feaafc81
Abdulrahman <AbdulrahmanAlhatti@gmail.com>
2022-11-23 23:15:55 +0300

    first commit

---
+++ ElectricCar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { }
+    }
+    class Car
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("Car is moving!");
+        }
+
+    }
+    class ElectricCar : Car
+    {
+        public override void Move()
+        {
+            base.Move();
+            Console.WriteLine("Electric Car is Environment Friendly");
+        }
+        public void Charge()
+        {
+            Console.WriteLine("Car is charged!");
+        }
+    }
+
+}
